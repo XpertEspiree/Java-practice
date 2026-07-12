adb3857231dc2ed732bf1e3bc09db28f889ee845
XpertEspire <xpertespire@gmail.com>
2026-07-12 22:54:06 +0545

    added rough for my java testing

---
+++ rough.java
@@ -0,0 +1,37 @@
+class rough{
+    public static void main(String[] args){
+       
+    int n=5;
+    byte by=66;
+    short sh=797;
+    double de=98908;
+
+    float f=7.8f;
+    long l=783983l;
+
+    char ch='y';
+    //System.out.println(ch);
+    String s="Fuck You";
+    //System.out.println(s);
+
+    boolean bl=true;
+  //  System.out.println(bl);
+     
+
+     int a=20-10+5, b=5+3, c=7;
+     if(a>b && a>c)
+     System.out.println("A has Greater Number");
+    
+    else if(b>a && b>c)
+    System.out.println("B has Greater Number");
+
+    else
+    System.out.println("C has Greater Number");
+
+    /* default:
+    System.out.println("Invalid Process");
+*/
+
+
+    }
+}
