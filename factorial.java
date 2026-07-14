778774a1b16e0b0c8012e084b50ec9dc4350585d
XpertEspire <xpertespire@gmail.com>
2026-07-14 09:18:56 +0545

    Added one more features

--- factorial.java
+++ factorial.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
+class factorial{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the Number: ");
+		int n=sc.nextInt();
+   
+		long fact=1;
 
-class factorial {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+		for(int i=1; i<=n; i++){
+			fact*=i;
+		}
 
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        long fact = 1;
-
-        for (int i = 1; i <= n; i++) {
-            fact = fact * i;
-        }
-
-        System.out.println("Factorial of " + n + " = " + fact);
-
-        sc.close();
-    }
+System.out.println("The factorial of " +n+ " is " +fact);
+		sc.close();
+	}
 }
