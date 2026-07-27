3436d58854346355e136ea38d3e8b2d655cbd44c
Bad Coder <xpertespire@gmail.com>
2026-07-27 09:19:27 +0545

    Add files via upload

---
+++ leo.java
@@ -0,0 +1,7 @@
+public class leo {
+    public static void main(String[] args) {
+        //Creating an object of the Car class
+        Car myCar = new Car();
+        // Assigning values of the object\
+    }
+}
